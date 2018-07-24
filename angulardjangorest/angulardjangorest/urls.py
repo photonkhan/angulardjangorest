f58f38b563946139c38e643ae32603eb56e75d4e
Shabuktagin Photon Khan <you@example.com>
2018-07-24 20:33:23 +0600

    preparing the api

--- angulardjangorest/angulardjangorest/urls.py
+++ angulardjangorest/angulardjangorest/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('angular.urls')),
     url(r'^angular/', include('angular.urls')),
+    url(r'^api/', include('api.urls')),
 ]
--- angulardjangorest/api/views.py
+++ angulardjangorest/api/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'header' : 'Angular with Djano Rest API'
+    }
+    return render(request, 'api/index.html', context)
+
