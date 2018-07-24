cde4a033115eec5147383b897b4780340721b922
Shabuktagin Photon Khan <you@example.com>
2018-07-24 20:15:40 +0600

    Angular JS work done

--- angulardjangorest/angular/static/js/direct.js
+++ angulardjangorest/angular/static/js/direct.js
@@ -1,3 +1,5 @@
 app.controller('directCtrl', function($scope){
-
+    $scope.pressedKey = function(){
+        console.log($scope.money);
+    }
 });
---
+++ angulardjangorest/angular/static/js/directive.js
@@ -0,0 +1,6 @@
+app.directive('directTemplate', function(){
+    return {
+        restrict: 'AEC',
+        templateUrl: '/static/html/directive.html'
+    }
+})
